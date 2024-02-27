cbf035d1f26a4263b43989fc05ea189274fb2905
congminhtran1802 <congminhtran42@gmail.com>
2024-02-27 13:34:40 +0700

    nothing

--- src/main/java/com/backend/stayEasy/api/AuthAPI.java
+++ src/main/java/com/backend/stayEasy/api/AuthAPI.java
@@ -33,6 +33,7 @@ public class AuthAPI {
 
 	@PostMapping("/register")
 	public ResponseEntity<SignInResponse> register(@RequestBody SignUpRequest request) {
+		System.out.println(request);
 		return ResponseEntity.ok(service.register(request));
 	}
 
