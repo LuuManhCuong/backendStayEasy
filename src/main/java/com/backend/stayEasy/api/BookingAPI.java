df88fe9734c8c42ea22816ca5819d2b1eee1bf52
Nguyenlocit20a1b <loc51936@donga.edu.vn>
2024-03-08 10:03:41 +0700

    Booking-and-payment(send email cancel payment , update API)

--- src/main/java/com/backend/stayEasy/api/BookingAPI.java
+++ src/main/java/com/backend/stayEasy/api/BookingAPI.java
@@ -1,14 +1,13 @@
 package com.backend.stayEasy.api;
 
 import com.backend.stayEasy.convertor.BookingConverter;
-
 import com.backend.stayEasy.dto.BookingDTO;
 import com.backend.stayEasy.dto.PaymentDTO;
-import com.backend.stayEasy.repository.BookingRepository;
+import com.backend.stayEasy.entity.Mail;
 import com.backend.stayEasy.sevice.BookingService;
+import com.backend.stayEasy.sevice.IMailService;
 import com.backend.stayEasy.sevice.PaymentBillService;
 import com.backend.stayEasy.sevice.PaypalService;
-import com.backend.stayEasy.sevice.PropertyService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paypal.api.payments.Links;
@@ -28,53 +27,59 @@ import java.util.UUID;
 @CrossOrigin
 @RequestMapping("/api/v1/stayeasy/booking")
 public class BookingAPI {
-    @Autowired
-    private BookingService bookingService;
-    @Autowired
-    private PropertyService propertyService;
-    @Autowired
-    private BookingRepository bookingRepository;
-
-    @Autowired
-    private BookingConverter bookingConverter;
+    public static final String SUCCESS_URL = "/paypal/success";
+    public static final String CANCEL_URL = "/paypal/cancel";
     @Autowired
     PaypalService service;
     @Autowired
     PaymentBillService paymentService;
-    public static final String SUCCESS_URL = "/paypal/success";
-    public static final String CANCEL_URL = "/paypal/cancel";
+    @Autowired
+    private BookingService bookingService;
+    @Autowired
+    private IMailService mailService;
+    @Autowired
+    private BookingConverter bookingConverter;
     private UUID bookingId;
+    private boolean emailSent = false;
+
+    // chi admin moi xem duoc
     @GetMapping(value = "")
-    public ResponseEntity<List<BookingDTO>> returnMyActiveBookings(){
+    public ResponseEntity<List<BookingDTO>> returnMyActiveBookings() {
         return ResponseEntity.ok().body(bookingService.findAll());
     }
 
+    // get danh sach booking user
     @GetMapping("/traveler/{id}")
     public ResponseEntity<List<BookingDTO>> getBookingById(@PathVariable("id") UUID id) {
         return ResponseEntity.ok().body(bookingService.returnMyBookings(id));
     }
+
+    // get danh sach booking cho tung listing
     @GetMapping("/listing/{id}")
     public ResponseEntity<List<BookingDTO>> getBookingOfListing(@PathVariable("id") UUID id) {
         return ResponseEntity.ok().body(bookingService.returnListingBookings(id));
     }
-//    @PutMapping("/changer-booking/{id}")
-//    public Booking updateBooking(@PathVariable("id") UUID id, @RequestBody Booking booking) {
-//        return bookingService.updateBooking(id, booking);
-//    }
+    /*  @PutMapping("/changer-booking/{id}")
+        public Booking updateBooking(@PathVariable("id") UUID id, @RequestBody Booking booking) {
+           return bookingService.updateBooking(id, booking);
+       } */
 
+
+    // huy booking (check ngay truoc checkin 1 ngay  , return payment ,them vao bang paymnet bill , cap nhat trang thai vot  booking)
+    // refund payment (lay stk cua user da thanh toan va refund)
     @DeleteMapping("/traveler-cancel/{id}")
     public void deleteBooking(@PathVariable("id") UUID id) {
         bookingService.deleteBooking(id);
     }
 
+    // create payment and update booking (check id  user isn't host )
     @PostMapping("/create")
     public ResponseEntity<String> createBooking(@RequestBody BookingDTO bookingParam) throws JsonProcessingException {
-
         if (bookingService.isRoomAvailable(bookingParam.getPropertyId(), bookingParam.getCheckIn(), bookingParam.getCheckOut())) {
             BookingDTO bookingDTO = bookingService.newBooking(bookingParam);
             // Lưu booking vào cơ sở dữ liệu
             BookingDTO savedBooking = bookingService.crateBooking(bookingConverter.toEntity(bookingDTO)); //  phương thức saveBooking
-            if(savedBooking != null) {
+            if (savedBooking != null) {
                 bookingId = savedBooking.getBookingId();
                 // Chuyển hướng đến PayPal để thanh town
                 String approvalUrl = payment(bookingParam);
@@ -90,11 +95,11 @@ public class BookingAPI {
             return new ResponseEntity<>("Phòng chưa thể đặt trong thời gian này", HttpStatus.BAD_REQUEST);
         }
     }
+
+    // payment with paypal
     public String payment(BookingDTO bookingDTO) {
         try {
-            Payment payment = service.createPayment(bookingDTO.getPrice(), bookingDTO.getCurrency(), bookingDTO.getIntent(),
-                    bookingDTO.getMethod(), bookingDTO.getDescription(), "http://localhost:3000/payment" + CANCEL_URL,
-                    "http://localhost:3000/payment" + SUCCESS_URL);
+            Payment payment = service.createPayment(bookingDTO.getPrice(), bookingDTO.getCurrency(), bookingDTO.getIntent(), bookingDTO.getMethod(), bookingDTO.getDescription(), "http://localhost:3000/payment" + CANCEL_URL, "http://localhost:3000/payment" + SUCCESS_URL);
             for (Links link : payment.getLinks()) {
                 if (link.getRel().equals("approval_url")) {
                     System.out.println("link" + link.getRel());
@@ -108,33 +113,72 @@ public class BookingAPI {
         return "";
     }
 
+    // return success payment
     @GetMapping(value = SUCCESS_URL)
-    public  ResponseEntity <List<PaymentDTO>> successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) throws JsonProcessingException {
+    public ResponseEntity<List<PaymentDTO>> successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId)  {
         try {
             Payment payment = service.executePayment(paymentId, payerId);
-            if (payment.getState().equals("approved")) {
-                // Update thong tin booking thanh true
-                bookingService.updateBookingStatus(bookingId , true);
+            if ("approved".equals(payment.getState())) {
+                // tranh null exception
+                bookingService.updateBookingStatus(bookingId, true);
                 paymentService.savePayment(payment, bookingId);
                 // Lưu thông tin payment
                 return ResponseEntity.ok().body(paymentService.findByPaymentId(paymentId));
             }
         } catch (PayPalRESTException e) {
-            bookingService.deleteBooking(bookingId);
-//            String message = " Thanh toán không thành công ! ";
-//            return  ResponseEntity.ok().body(new ObjectMapper().writeValueAsString(message));
-            System.out.println(e.getMessage());
+            sendEmailBooking(); // Send email notification about the cancellation
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        return null;
-            }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
 
+    // return when cancel payment
+    @GetMapping(value = CANCEL_URL)
+    public ResponseEntity<String> cancelPay()  {
+        if (!emailSent) {
+            // Gửi email nhắc thanh toán
+            sendEmailBooking();
+            emailSent = true;
+            String message = "Đã hủy thanh toán và gửi email nhắc thanh toán thành công.";
+            return ResponseEntity.ok(message);
+        } else {
+            String message = "Email đã được gửi đi trước đó.";
+            return ResponseEntity.ok(message);
+        }
+    }
+
+    // can code in booking service
+    private void sendEmailBooking() {
+        BookingDTO bookingDTO = bookingService.getBookingById(bookingId);
+        String paymentLinkTemplate = "http://localhost:3000/booking/%s?checkin=%s&checkout=%s&numGuest=%d";
+        System.out.println(bookingDTO.getNumOfGuest());
+        String paymentLink = String.format(paymentLinkTemplate, bookingDTO.getPropertyDTOS().getPropertyId(), bookingDTO.getCheckIn(), bookingDTO.getCheckOut(), bookingDTO.getNumOfGuest());
+        String subject = "Nhắc nhở thanh toán đặt phòng của bạn tại " + bookingDTO.getPropertyName();
+        String content = "test mail";
+        // set new mail
+        Mail mail = new Mail();
+        mail.setRecipient(bookingDTO.getUserDTOS().getEmail());
+        mail.setSubject(subject);
+        mail.setContent(content);
+        mailService.sendEmailPayment(mail, bookingDTO, paymentLink);
+//        mailService.sendBook(mail);
+    }
+
+    // get transaction detail
     @GetMapping("/lookup-transaction")
-    public ResponseEntity <List<PaymentDTO>>  LookupTrans (@RequestParam("paymentId") String paymentId) {
+    public ResponseEntity<List<PaymentDTO>> LookupTrans(@RequestParam("paymentId") String paymentId) {
         System.out.println("running");
         return ResponseEntity.ok().body(paymentService.findByPaymentId(paymentId));
     }
+
+    // Method payment host when traveler checkout ( khi user checkout thi AUTO PAYMENT  cho host theo stk đã dky trong user account )
+
 }
+
+
+
+
 
 
