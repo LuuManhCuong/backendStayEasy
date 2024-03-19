ff55f1e5c5baf19b7ce3aa59698a067c71520df0
Hồ Đức An <98076022+HoAn248@users.noreply.github.com>
2024-03-19 10:10:30 +0700

    update

---
+++ src/main/java/com/backend/stayEasy/repository/INotificationRepository.java
@@ -0,0 +1,15 @@
+package com.backend.stayEasy.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.backend.stayEasy.entity.Message;
+import com.backend.stayEasy.entity.Notification;
+
+
+public interface INotificationRepository extends JpaRepository<Notification, UUID>{
+    List<Notification> findByReceiverId(UUID receiverId);
+}
